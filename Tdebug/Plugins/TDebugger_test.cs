7aeb6e815986e1c1768743d2e0adf1ead509d7b8
OnZero <2834505535@qq.com>
2026-01-30 17:56:51 +0800

    tdebug

---
+++ Tdebug/Plugins/TDebugger_test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using TDebugger;
+
+public class TDebugger_test : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+    {
+        TDebug.Toggle = true;
+        TDebug.Log(string.Format("TDebugger.TDebug.Log(),TDebug.Toggle = {0}", TDebug.Toggle));
+        TDebug.LogWarning(string.Format("TDebugger.TDebug.LogWarning(),TDebug.Toggle = {0}", TDebug.Toggle));
+        TDebug.LogError(string.Format("TDebugger.TDebug.LogError(),TDebug.Toggle = {0}", TDebug.Toggle));
+
+        TDebug.Toggle = true;
+        TDebug.Log(string.Format("TDebugger.TDebug.Log(),TDebug.Toggle = {0}", TDebug.Toggle));
+        TDebug.Log($"sdfsdf{233333}");
+        TDebug.LogWarning(string.Format("TDebugger.TDebug.Log(),TDebug.LogWarning = {0}", TDebug.Toggle));
+        TDebug.LogError(string.Format("TDebugger.TDebug.LogError(),TDebug.Toggle = {0}", TDebug.Toggle));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
